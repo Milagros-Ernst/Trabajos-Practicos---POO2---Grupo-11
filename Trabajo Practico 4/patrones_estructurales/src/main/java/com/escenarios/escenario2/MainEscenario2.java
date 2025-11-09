b16b5a7c3a7913c446f27e84a9061ad2e89a2fc4
Juanma <veronjuan111@gmail.com>
2025-11-09 04:12:40 -0300

    Borré una variable que no se usaba en el escenario 2.

--- Trabajo Practico 4/patrones_estructurales/src/main/java/com/escenarios/escenario2/MainEscenario2.java
+++ Trabajo Practico 4/patrones_estructurales/src/main/java/com/escenarios/escenario2/MainEscenario2.java
@@ -1,13 +1,11 @@
 package com.escenarios.escenario2;
 
-import java.nio.file.Path;
-
 public class MainEscenario2 {
     public static void main(String[] args) {
         ReporteFiscalFacade facade = new ReporteFiscalFacade(
                 "jdbc:mysql://localhost:3306/clientes",
                 "TOKEN_AFIP_12345"
         );
-        Path reporte = facade.generarReportePDF("CLI-001");
+        facade.generarReportePDF("CLI-001");
     }
 }

dabd1b0217131a288f005727f14f73760fb9dc20
Juanma <veronjuan111@gmail.com>
2025-11-09 03:23:02 -0300

    Cambié el nombre de la carpeta "reportes" a "patrones_creacionales" del Trabajo Práctico 3.

--- Trabajo Practico 3/patrones_creacionales/src/main/java/com/org/App.java
+++ Trabajo Practico 3/patrones_creacionales/src/main/java/com/org/App.java
@@ -1,4 +1,4 @@
-package com.org.app;
+package com.org;
 
 import com.org.singleton.GestorConfiguracion;
 import com.org.factory.*;
--- patrones_estructurales/src/main/java/com/escenarios/escenario1/Componente.java
+++
@@ -1,6 +0,0 @@
-package com.escenarios.escenario1;
-
-public interface Componente {
-    double obtenerPrecio();
-    void mostrarDetalle(String indent);
-}
